64655a818c5afc55659f991fbb16ee71ae683e1c
smalaca <ab13.krakow@gmail.com>
2021-04-22 18:53:36 +0200

    FizzBuzz - GREEN - should return Fizz when number dividable by three

--- src/main/java/com/smalaca/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/smalaca/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,10 @@ package com.smalaca.tdd.fizzbuzz;
 
 public class FizzBuzz {
     String covert(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
         return String.valueOf(number);
     }
 }
