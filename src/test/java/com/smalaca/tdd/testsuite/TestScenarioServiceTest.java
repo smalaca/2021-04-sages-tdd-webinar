ea444625150b7a4f1cfddc322f9a20429c63e54d
smalaca <ab13.krakow@gmail.com>
2021-04-22 19:19:37 +0200

    TestScenarioServiceTest - REFACT - move test scenario name to constant

--- src/test/java/com/smalaca/tdd/testsuite/TestScenarioServiceTest.java
+++ src/test/java/com/smalaca/tdd/testsuite/TestScenarioServiceTest.java
@@ -7,21 +7,23 @@ import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
 
 public class TestScenarioServiceTest {
-/*
-Wymagania:
-1. TestScenarioService umożliwia zapisanie TestScenario dla danego Autora gdy TestScenario jest poprawny.
-2. TestScenario jest poprawny gdy zawiera co najmniej jedną asercję oraz fragment wywołujący kod produkcyjny.
-3. TestScenario może zostać zapisany gdy nie istnieje TestScenario z taką samą nazwą
- */
+    private static final String TEST_SCENARIO_NAME = "some great name of test scenario";
+
+    /*
+            Wymagania:
+            1. TestScenarioService umożliwia zapisanie TestScenario dla danego Autora gdy TestScenario jest poprawny.
+            2. TestScenario jest poprawny gdy zawiera co najmniej jedną asercję oraz fragment wywołujący kod produkcyjny.
+            3. TestScenario może zostać zapisany gdy nie istnieje TestScenario z taką samą nazwą
+             */
     private final TestScenarioRepository repository = mock(TestScenarioRepository.class);
     private final TestScenarioService service = new TestScenarioService(repository);
 
     @Test
     void shouldSaveTestScenario() {
         //given
-        givenNotExistingTestScenarioWithName("shouldSaveTestScenario");
+        givenNotExistingTestScenarioWithName(TEST_SCENARIO_NAME);
         Author author = new Author();
-        TestScenario testScenario = new TestScenario("shouldSaveTestScenario");
+        TestScenario testScenario = new TestScenario(TEST_SCENARIO_NAME);
         testScenario.add(new TestAssertion());
         testScenario.add(new GivenPart());
 
