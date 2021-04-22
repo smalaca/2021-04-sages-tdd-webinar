a1e830163eb6368a5a4637125b8f29b07e700b86
smalaca <ab13.krakow@gmail.com>
2021-04-22 10:42:48 +0200

    Commit with requirements

---
+++ src/test/java/com/smalaca/tdd/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,34 @@
+package com.smalaca.tdd.fizzbuzz;
+
+public class FizzBuzzTest {
+/*
+Wymagania:
+1. Gdy jest przekazana liczba (int) zwracamy tą liczbę w postaci String'a.
+2. Gdy liczba jest podzielna przez 3 zwracamy Fizz.
+3. Gdy liczba jest podzielna przez 5 zwracamy Buzz.
+4. Gdy liczba jest podzielna przez 3 oraz 5 zwracamy FizzBuzz.
+
+INPUT   | OUTPUT
+1       | 1
+2       | 2
+3       | Fizz
+4       | 4
+5       | Buzz
+6       | Fizz
+7       | 7
+8       | 8
+9       | Fizz
+10      | Buzz
+11      | 11
+12      | Fizz
+13      | 13
+14      | 14
+15      | FizzBuzz
+16      | 16
+17      | 17
+18      | Fizz
+19      | 19
+20      | Buzz
+*/
+
+}
---
+++ src/test/java/com/smalaca/tdd/testsuite/TestScenarioServiceTest.java
@@ -0,0 +1,10 @@
+package com.smalaca.tdd.testsuite;
+
+public class TestScenarioServiceTest {
+/*
+Wymagania:
+1. TestScenarioService umożliwia zapisanie TestScenario dla danego Autora gdy TestScenario jest poprawny.
+2. TestScenario jest poprawny gdy zawiera co najmniej jedną asercję oraz fragment wywołujący kod produkcyjny.
+3. TestScenario może zostać zapisany gdy nie istnieje TestScenario z taką samą nazwą
+ */
+}
