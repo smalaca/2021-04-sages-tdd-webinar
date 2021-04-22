34b1b0a58a74b611ad12055246884174de8a7f74
smalaca <ab13.krakow@gmail.com>
2021-04-22 18:54:53 +0200

    FizzBuzz - GREEN - should return Buzz when number dividable by five

--- src/main/java/com/smalaca/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/smalaca/tdd/fizzbuzz/FizzBuzz.java
@@ -6,6 +6,10 @@ public class FizzBuzz {
             return "Fizz";
         }
 
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+
         return String.valueOf(number);
     }
 }
